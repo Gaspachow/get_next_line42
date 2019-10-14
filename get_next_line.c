2f119140c31b762fd5497861fb1a53bb6cfc3daf
Gaspard SMETS <gsmets@student.42.fr>
2019-10-14 19:17:04 +0200

    Multiple norme and compilation fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,21 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 12:20:07 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/14 18:36:03 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/14 19:16:32 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE
+#define BUFFER_SIZE 256
 
 int		get_next_line(int fd, char **line)
 {
-	unsigned char	buff[BUFFER_SIZE + 1];
+	char	buff[BUFFER_SIZE + 1];
 	static char		*str;
 	size_t			ret;
 	char			*tmp;
 
-	while (ret = read(fd, *buf, BUFFER_SIZE))
+	while ((ret = read(fd, buff, BUFFER_SIZE)))
 	{
 		buff[ret] = '\0';
 		if(!(tmp = ft_strjoin(str, buff)))
@@ -28,9 +28,9 @@ int		get_next_line(int fd, char **line)
 		free(str);
 		str = tmp;
 	}
-	if (!ret && str[0] = '\0')
+	if (!ret && str[0] == '\0')
 		return (0);
-	if (*line = create_line(str, line, fd))
+	if ((*line = create_line(str, line, fd)))
 		return (1);
 	else
 		return (-1);
@@ -47,10 +47,11 @@ char	*create_line(char *str, char **line, int fd)
 	if (str[len] == '\n')
 	{
 		newline = malloc((len + 1) * sizeof(char));
-		memmove(newline, str, len);
+		ft_memmove(newline, str, len);
 		newline[len] = '\0';
 		return (newline);
 	}
 	else
 		get_next_line(fd, line);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 14:43:35 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/14 18:16:51 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/14 19:04:01 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 size_t	ft_strlen(const char *str);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *src);
+int		get_next_line(int fd, char **line);
+char	*create_line(char *str, char **line, int fd);
 
 #endif
