15c31b75037731d4404f4b846b94476d5ac70d24
Gaspard SMETS <gsmets@student.42.fr>
2019-10-16 13:13:49 +0200

    Almost working - bugs when file ends whitout \n

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 12:20:07 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/15 14:44:39 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/16 13:07:30 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1
 
 int		get_next_line(int fd, char **line)
 {
@@ -19,23 +19,26 @@ int		get_next_line(int fd, char **line)
 	static char		*str;
 	size_t			ret;
 	char			*tmp;
-
-	if ((ret = read(fd, buff, BUFFER_SIZE)))
+	while ((ret = read(fd, buff, BUFFER_SIZE)))
 	{
 		buff[ret] = '\0';
-		if(!(tmp = ft_strjoin(buff, str)))
+		if(!(tmp = ft_strjoin(str, buff)))
 			return (-1);
 		free(str);
 		str = tmp;
 	}
 	if (!ret && str[0] == '\0')
+	{
 		return (0);
+	}
 	if ((*line = create_line(&str, line, fd)))
 	{
 		return (1);
 	}
 	else
+	{
 		return (-1);
+	}
 }
 
 char	*create_line(char **str, char **line, int fd)
@@ -43,16 +46,17 @@ char	*create_line(char **str, char **line, int fd)
 	size_t	len;
 	char	*newline;
 	char	*tmp;
-
 	len = 0;
-	while (*str[len] != '\n' && *str[len] != '\0')
+	while ((*str)[len] != '\n' && (*str)[len] != '\0')
+	{
 		len++;
-	if (*str[len] == '\n')
+	}
+	if ((*str)[len] == '\n')
 	{
 		newline = malloc((len + 1) * sizeof(char));
 		ft_memmove(newline, *str, len);
 		newline[len] = '\0';
-		tmp = ft_strdup(str[len + 1]);
+		tmp = ft_strdup(&(*str)[len + 1]);
 		free(*str);
 		*str = tmp;
 		return (newline);
