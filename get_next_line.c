54c72a2390638423e39f0795bb59f2d1ce2eaa32
Gaspard SMETS <gsmets@student.42.fr>
2019-10-21 20:13:38 +0200

    norme GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 12:20:07 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/21 19:37:57 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/21 20:13:23 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 }
 
-
 char	*create_line(char **str)
 {
 	size_t	len;
@@ -51,18 +50,18 @@ char	*create_line(char **str)
 	while ((*str)[len] != '\n' && (*str)[len] != '\0')
 		len++;
 	if ((*str)[len] == '\0')
-		return(last_line(str));
+		return (last_line(str));
 	if (!(newline = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	ft_memmove(newline, *str, len);
 	newline[len] = '\0';
 	tmp = ft_substr(*str, len, (ft_strlen(*str) - (len + 1)));
 	free(*str);
-		*str = tmp;
+	*str = tmp;
 	return (newline);
 }
 
-char	*last_line(char	**str)
+char	*last_line(char **str)
 {
 	char	*newline;
 
@@ -71,43 +70,6 @@ char	*last_line(char	**str)
 	*str = NULL;
 	return (newline);
 }
-
-
-// char	*create_line(char **str)
-// {
-// 	size_t	len;
-// 	char	*newline;
-// 	char	*tmp;
-
-// 	len = 0;
-// 	while ((*str)[len] != '\n' && (*str)[len] != '\0')
-// 		len++;
-// 	 if (len && (*str)[len] == '\n' && (*str)[len + 1] == '\0')
-// 	 	return (last_string(str, len));
-// 	if (!(newline = malloc((len + 1) * sizeof(char))))
-// 		return (NULL);
-// 	ft_memmove(newline, *str, len);
-// 	newline[len] = '\0';
-// 	tmp = ft_substr(*str, len + 1, (ft_strlen(*str) - (len + 1)));
-// 	free(*str);
-// 	*str = tmp;
-// 	return (newline);
-// }
-
-// char	*last_string(char **str, size_t len)
-// {
-// 	char	*newline;
-
-// 	if (!(newline = malloc((len + 1) * sizeof(char))))
-// 		return (NULL);
-// 	ft_memmove(newline, *str, len);
-// 	newline[len] = '\0';
-// 	free(*str);
-// 	*str = malloc(2 * sizeof(char));
-// 	(*str)[0] = '\n';
-// 	(*str)[1] = '\0';
-// 	return (newline);
-// }
 
 int		check_nl(char *str)
 {
