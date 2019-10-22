cac10f011b03166374b79a485c2cedb20e424880
Gaspard SMETS <gsmets@student.42.fr>
2019-10-22 15:37:18 +0200

    version of GNL that ignores a last empty line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 12:20:07 by gsmets            #+#    #+#             */
-/*   Updated: 2019/10/22 14:21:29 by gsmets           ###   ########.fr       */
+/*   Updated: 2019/10/22 15:33:23 by gsmets           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ char	*create_line(char **str)
 	len = 0;
 	while ((*str)[len] != '\n' && (*str)[len] != '\0')
 		len++;
-	if ((*str)[len] == '\0')
+	if ((*str)[len] == '\0' || (*str)[len + 1] == '\0') // -------- "|| (*str)[len + 1] == '\0'" to ignore last newline
 		return (last_line(str));
 	if (!(newline = malloc((len + 1) * sizeof(char))))
 		return (NULL);
